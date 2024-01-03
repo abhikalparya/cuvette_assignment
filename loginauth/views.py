f8092a4ab500ada6bdb41dc5e15c502bfadfc4b8
Abhikalp Arya <abhikalp.arya@gmail.com>
2024-01-03 16:17:23 +0530

    implemented login and logout

--- loginauth/views.py
+++ loginauth/views.py
@@ -1,19 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is our homepage!")
+    return render(request, 'loginauth/home.html')
 
 def profile(request):
-    return HttpResponse("This is our profile page!")
+    return render(request, 'loginauth/profile.html')
 
-def login(request):
-    return HttpResponse("This is our login page!")
+def login_user(request):
 
-def register(request):
-    return HttpResponse("This is our registration page!")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-def logout(request):
-    return HttpResponse("This is our logout page!")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, f'{username}, You are logged in.')
+            return redirect("/")
+        else:
+            messages.info(request, 'Wrong password or username')
+            return redirect('login')
+        
+    return render(request, 'loginauth/login_page.html')
+
+def register_user(request):
+    return render(request, 'loginauth/register_page.html')
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'You logged out successfully')
+    return redirect('login')
